e3e509b819d8c45796bb46ee9813f49abcf5660a
Yuki Kodama <endflow.net@gmail.com>
2015-06-13 14:09:08 +0900

    Use temporary files and handle expected errors

--- unlock.rb
+++ unlock.rb
@@ -1,36 +1,51 @@
 #!/usr/bin/env ruby
 
 require 'yaml'
+require 'tempfile'
 require 'net/http'
 require 'net/ftp'
 
-cfg = YAML.load_file File.expand_path '../config.yml', __FILE__
+cfg = nil
+begin
+  cfg = YAML.load_file File.expand_path '../config.yml', __FILE__
+rescue Errno::ENOENT
+  warn 'ERROR: config.yml not found'
+  exit 1
+end
 
 Net::FTP.open cfg['host'] do |ftp|
   ftp.login cfg['username'], cfg['password']
   ftp.passive = true
-  ftp.getbinaryfile '.htaccess', '_htaccess'
 
-  lines = nil
-  open('_htaccess') do |f|
-    lines = f.readlines
+  lines = Tempfile.open 'lwu' do |tmp|
+    ftp.getbinaryfile '.htaccess', tmp.path
+    tmp.readlines
   end
 
-  pos = nil
+  current_ip = Net::HTTP.get('ipinfo.io', '/ip').strip!
+  lines.each do |line|
+    if line.include? current_ip
+      puts "Your IP address #{current_ip} is already allowed in .htaccess file, exit"
+      exit
+    end
+  end
+
+  marker_pos = nil
   lines.each_with_index do |line, i|
     if line.start_with? 'Deny  from all'
-      pos = i
+      marker_pos = i
       break
     end
   end
 
-  ip = Net::HTTP.get('ipinfo.io', '/ip').strip!
-  lines.insert pos + 1, "Allow from #{ip}\n"
+  lines.insert marker_pos + 1, "Allow from #{current_ip}\n"
 
-  open('_htaccess_updated', 'w') do |f|
-    f.write lines.join('')
+  updated_path = Tempfile.open 'lwu' do |tmp|
+    tmp.write lines.join('')
+    tmp.path
   end
 
-  # ftp.delete '.htaccess'
-  ftp.putbinaryfile '_htaccess_updated', '.htaccess'
+  ftp.putbinaryfile updated_path, '.htaccess'
+
+  puts "Your IP address #{current_ip} is now allowed"
 end
